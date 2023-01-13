4040015d01c365852feb3438e05d914ab06c8956
Billyngwa <billytatangwa5@gmail.com>
2023-01-13 19:38:54 +0100

    added show password functionality

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,61 +1,38 @@
-const signInCheckBox = document.querySelector('input.sign_in_show');
-const signUpCheckBox = document.querySelector('input.sign_up_show');
-let signUpPasswordField  = document.querySelector('.sign_up_passwordField');
-let signInPasswordField  = document.querySelector('.sign_in_passwordField');
+const signInCheckBox = document.querySelector('.check_me');// Getting the checkbox as an object
+let signUpPasswordField  = document.querySelector('.sign_up_passwordField');// Getting password field for signup as object
+let signInPasswordField  = document.querySelector('.sign_in_passwordField');// getting password for sign in as object
 let userName = document.querySelector('.name_holder');
 
 let email = document.querySelector('.email_holder');
-// let passWord = document.querySelector('.password_holder');
 let signupButton = document.querySelector('.signup_btn');
 let formElement = document.querySelector('form');
 
-// checkBoxes.forEach(checkbox => {
-//       checkbox.addEventListener('click', event => {
-//             checkbox.classList.toggle('check');
-//             signInPasswordField.classList.toggle('check');
-//             signUpPasswordField.classList.toggle('check');
-//       });
-//       if(signInPasswordField.classList.contains('check') && signUpPasswordField.classList.contains('check')){
-//             signInPasswordField.type  = 'text';
-//             signUpPasswordField.type = 'text';
-//       }else{
-//             signInPasswordField.type  = 'password';
-//             signUpPasswordField.type = 'password';
-//       }
-// });
-
-// signUpCheckBox.addEventListener('click', event => {
-//       signUpCheckBox.classList.toggle('check');
-//       signUpPasswordField.classList.toggle('check');
-//     if(signUpPasswordField.classList.contains('check')){
-//       signUpPasswordField.type = 'text';
-//     }else{
-//       signUpPasswordField.type = 'password';
-//     }
-// });
-
-
-
+//this block of code function to show the password written down by the user
+signInCheckBox.addEventListener('click', action =>{
+  // action.preventDefault();
+  signInCheckBox.classList.toggle('reveal_password');
+  signInPasswordField.classList.toggle('reveal_password');
+    if(signInPasswordField.classList.contains('reveal_password')){
+        signInPasswordField.type = 'text';
+    }else{
+      signInPasswordField.type = 'password';
+    }
+});
+// End of show password
 
 let arrayObject = [];
 signupButton.addEventListener('click', event =>{
       event.preventDefault();
+      const usersInLocalStorage = JSON.parse(localStorage.getItem(arrayObject));
       const createdAccount = {
             Name:userName.value,
             Email: email.value,
             password :signUpPasswordField.value
       };
       arrayObject.push(createdAccount);
+      arrayObject.concat(usersInLocalStorage);
       window.localStorage.setItem('User',JSON.stringify(arrayObject));
       formElement.reset();
+      console.log(usersInLocalStorage);
 });
 
-signInCheckBox.addEventListener('click', event => {
-      signInCheckBox.classList.toggle('check');
-      signInPasswordField.classList.toggle('check');
-    if(signInPasswordField.classList.contains('check')){
-      signInPasswordField.type = 'text';
-    }else{
-      signInPasswordField.type = 'password';
-    }
-});
